e7607a1a5b6e18e90e4bb478546e98671acf27b2
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-03 23:49:39 -0300

    Esqueleto do Spring Initializr

---
+++ src/main/java/com/derso/cursoudemy/DersoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.derso.cursoudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DersoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DersoSpringBootApplication.class, args);
+	}
+
+}
