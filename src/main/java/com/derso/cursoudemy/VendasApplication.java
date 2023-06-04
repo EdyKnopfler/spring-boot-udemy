428f223326c63586dcbb41a01a8ad81389b0aa93
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-04 01:20:25 -0300

    Config via application.properties

--- src/main/java/com/derso/cursoudemy/VendasApplication.java
+++ src/main/java/com/derso/cursoudemy/VendasApplication.java
@@ -3,6 +3,7 @@ package com.derso.cursoudemy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 	
+	// Via classe de configuração
 	@Autowired
 	@Qualifier("applicationName")
 	private String applicationName;
 	
+	// Via application.properties
+	@Value("${application.version}")
+	private String applicationVersion;
+	
 	@GetMapping("/hello")
 	public String helloPaidegua() {
-		return "Hellô Paidégua!!! Esta é a aplicação " + applicationName;
+		return "Hellô Paidégua!!! Esta é a aplicação "
+				+ applicationName + " (" + applicationVersion + ")";
 	}
 	
 	public static void main(String[] args) {
