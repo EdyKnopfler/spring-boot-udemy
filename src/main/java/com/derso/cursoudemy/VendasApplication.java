57f5a5f518d7116692898383b3c3bff15ada8ec6
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-04 00:51:01 -0300

    Exemplo de configuração por classe

--- src/main/java/com/derso/cursoudemy/VendasApplication.java
+++ src/main/java/com/derso/cursoudemy/VendasApplication.java
@@ -1,6 +1,8 @@
 // Quaisquer pacotes desta aplicação devem estar daqui para baixo!
 package com.derso.cursoudemy;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 	
+	@Autowired
+	@Qualifier("applicationName")
+	private String applicationName;
+	
 	@GetMapping("/hello")
 	public String helloPaidegua() {
-		return "Hellô Paidégua!!!";
+		return "Hellô Paidégua!!! Esta é a aplicação " + applicationName;
 	}
 	
 	public static void main(String[] args) {
---
+++ src/main/java/com/derso/cursoudemy/configuration/WebConfiguration.java
@@ -0,0 +1,14 @@
+package com.derso.cursoudemy.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+	
+	@Bean(name="applicationName")
+	public String applicationName() {
+		return "Sistema de Vendas";
+	}
+
+}
